fc70ab4cfe5c811e12e88d7972de7d2c1f741232
tohweizhong <tohweizhong@hotmail.com>
2019-04-12 14:35:02 +0800

    stats finance, st2334

---
+++ 05 Statistical finance/Building a Data Science Portfolio_ Setting Up a Blog – Dataquest_files/style.css
@@ -0,0 +1,40 @@
+/* Search & Filter Styles */
+body
+{
+	display:block;
+}
+.searchandfilter p
+{
+	margin-top: 1em;
+	display:inline-block;
+}
+.searchandfilter ul
+{
+	display:inline-block;
+}
+.searchandfilter li
+{
+	list-style: none;
+	display:inline-block;
+	padding-right:10px;
+}
+
+/* If in a widget area make a single column by adding display block  */
+.widget-area .searchandfilter li, .widget-area .searchandfilter p
+{
+	display:block;
+}
+.searchandfilter label
+{
+	display:block;
+}
+.searchandfilter h4
+{
+	margin:15px 0;
+	font-size:16px;
+}
+
+.searchandfilter ul > li > ul:not(.children)
+{
+	margin-left:0;
+}
